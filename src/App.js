9fe06b7f43e155db015255b2cb68b4a538dbc13f
um7a <um7a@users.noreply.github.com>
2023-02-26 15:56:57 +0900

    feat: Replace BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import NotFound from "./NotFound";
 import SvgIcons from "./SvgIcons";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path={`/ui-design`} element={<Home />} />
-        <Route path={`/ui-design/svg-icons`} element={<SvgIcons />} />
+        <Route path={`/ui-design/#/svg-icons`} element={<SvgIcons />} />
         <Route path={`/*`} element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
